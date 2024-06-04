2f270e94e4f7f5cc6b60f3119afa9bc266acec58
hideo2012 <hideo.chou@gmail.com>
2024-06-04 07:08:09 +0000

    add turbo-stream for edit cat

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -28,7 +28,7 @@ class CatsController < ApplicationController
     @cat = Cat.new(cat_params)
 
     if @cat.save
-      redirect_to @cat, notice: "Cat was successfully created."
+      redirect_to @cat, notice: "ねこを登録しました。"
     else
       render :new, status: :unprocessable_entity
     end
@@ -37,7 +37,8 @@ class CatsController < ApplicationController
   # PATCH/PUT /cats/1
   def update
     if @cat.update(cat_params)
-      redirect_to @cat, notice: "Cat was successfully updated.", status: :see_other
+#      redirect_to @cat, notice: "ねこを更新しました。", status: :see_other
+      flash.now.notice = "ねこを更新しました。"
     else
       render :edit, status: :unprocessable_entity
     end
